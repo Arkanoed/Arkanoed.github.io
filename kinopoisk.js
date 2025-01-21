144289c62ca70ed49bf7fb90389edd805257c5c1
Arkanoed <arcaska@yandex.ru>
2025-01-21 13:01:53 +0300

    Update kinopoisk.js

--- kinopoisk.js
+++ kinopoisk.js
@@ -1,34 +1,38 @@
 (function () {
-    console.log("Плагин 'Кнопка перехода на КиноПоиск' запущен");
+    console.log("Плагин для кликабельного рейтинга КиноПоиск подключен");
 
-    alert("Плагин загружен!");
+    // Функция, которая добавляет стиль к рейтингу КиноПоиска
+    function styleRating() {
+        const kpRating = document.querySelector('.info__rate span[data-type="kp"]'); // Ищем элемент с рейтингом КиноПоиска
 
-    Lampa.Listener.follow('card', function (event) {
-        console.log("Карточка фильма открыта:", event);
-
-        var kpRating = document.querySelector('.info__rate span[data-type="kp"]');
         if (kpRating) {
-            console.log("Рейтинг КиноПоиска найден:", kpRating.textContent);
-
+            // Убедимся, что элемент существует, и добавим стиль
             kpRating.style.fontWeight = 'bold';
-            kpRating.style.color = 'orange';
-            kpRating.style.cursor = 'pointer';
+            kpRating.style.color = 'orange';  // Оранжевый цвет
+            kpRating.style.cursor = 'pointer';  // Курсор в виде руки, чтобы указать на кликабельность
 
+            // Добавляем обработчик клика
             kpRating.addEventListener('click', function () {
-                var kpId = kpRating.dataset.id || kpRating.parentElement.dataset.id;
+                const kpId = kpRating.dataset.id || kpRating.closest('.card').dataset.id; // Получаем ID фильма из data-id
 
                 if (kpId) {
-                    var kpLink = 'https://www.kinopoisk.ru/film/' + kpId;
-                    console.log("Переход по ссылке:", kpLink);
-                    window.open(kpLink, '_blank');
+                    // Формируем ссылку на КиноПоиск
+                    const kpLink = `https://www.kinopoisk.ru/film/${kpId}/`;
+                    window.open(kpLink, '_blank'); // Открываем ссылку в новой вкладке
                 } else {
-                    console.warn("ID фильма не найден, ссылка не создана");
+                    console.warn("ID фильма не найден, ссылка на КиноПоиск не была создана.");
                 }
             });
         } else {
-            console.warn("Рейтинг КиноПоиска не найден в карточке фильма");
+            console.warn("Рейтинг КиноПоиска не найден на карточке фильма");
         }
+    }
+
+    // Следим за событиями на карточке фильма
+    Lampa.Listener.follow('card', function (event) {
+        console.log("Карточка фильма открыта:", event);
+        styleRating();  // Применяем стили к рейтингу
     });
 
-    console.log("Слушатель события для карточки установлен");
+    console.log("Плагин для рейтинга КиноПоиск работает");
 })();
